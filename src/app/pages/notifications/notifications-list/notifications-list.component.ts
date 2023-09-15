f64611de3f1f24a4f96feb1089c4ddf120a5622c
MohammedRazzak <50241203+MohammedRazzak@users.noreply.github.com>
2023-09-15 16:43:31 +0300

    add new notification popup

--- src/app/pages/notifications/notifications-list/notifications-list.component.ts
+++ src/app/pages/notifications/notifications-list/notifications-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NzModalService } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-notifications-list',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./notifications-list.component.scss']
 })
 export class NotificationsListComponent {
+  isVisible = false;
+  constructor(private modalService: NzModalService){
+
+  }
+
+  addNewNotification(){
+    this.isVisible = true;
+  }
+
+  handleCancel(){
+    this.isVisible = false;
+  }
+  
+  handleOk(){
+    this.isVisible = false;
+  }
 
 }
